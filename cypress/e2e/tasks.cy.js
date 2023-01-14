44e3dbbdedbf02d702e1c44222549b9449bde219
Elias Siqueira <eliaslsiqueira@gmail.com>
2023-01-14 01:15:55 -0400

    using faker

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,12 +1,14 @@
 /// <reference types="cypress" />
 
+import { faker } from '@faker-js/faker';
+
 describe('tarefas', () => {
 
     it('deve caastrar uma nova tarefa', () => {
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type('Ler um livro de Node.js')
+            .type(faker.music.songName())
 
         cy.contains('button', 'Create').click()
     })
