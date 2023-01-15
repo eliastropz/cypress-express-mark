5e9c86bb455d09a135941cbc2f1f26f8885f3118
Elias Siqueira <eliaslsiqueira@gmail.com>
2023-01-15 19:38:41 -0400

    allure reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,18 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
-    env:{
+    env: {
       apiUrl: 'http://localhost:3333'
     },
-    viewportHeight:1920,
-    viewportHeight:1080,
+    viewportHeight: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
